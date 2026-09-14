0caf017326f5965defabdc3fe479923b7de72713
Lucas stenros <lucas.stenros@outlook.com>
2026-09-14 16:11:49 +0200

    fungerande create och destroy

--- hash_table.c
+++ hash_table.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-#include "hash_table.h"
-
-typedef struct entry entry_t;
 typedef struct hash_table ioopm_hash_table_t;
+typedef struct entry entry_t;
+
 struct entry
 {
-  char *key;    // holds the key
-  int value;    // holds the value
+  char *key;     // holds the key
+  int value;     // holds the value
   entry_t *next; // points to the next entry (possibly NULL)
 };
 
 struct hash_table
 {
+  // DODGE: hard-coding number of buckets as 17.
+  // NOTE: addressing this dodge is optional.
   entry_t *buckets[17];
 };
 
-
-
-ioopm_hash_table_t *ioopm_hash_table_create(void) {
-  // Todo: Stub
-  return NULL;
+ioopm_hash_table_t *ioopm_hash_table_create()
+{
+  /// Allocate zeroed-out space for a ioopm_hash_table_t = 17 pointers to entry_t's
+  return calloc(1, sizeof(ioopm_hash_table_t));
 }
 
 void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
@@ -30,3 +30,4 @@ void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
     free (ht);
  return;
 }
+
