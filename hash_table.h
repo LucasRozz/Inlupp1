e5a23256a82160fc6ef938857a12e17bbc11f8ed
Lucas stenros <lucas.stenros@outlook.com>
2026-09-15 10:44:06 +0200

    uppdaterade insert, lookup

--- hash_table.h
+++ hash_table.h
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #pragma once
 
 /**
@@ -32,10 +33,11 @@ void ioopm_hash_table_insert(ioopm_hash_table_t *ht, char *key, int value);
 /// @param ht hash table operated upon
 /// @param key key to lookup
 /// @return the value mapped to by key (FIXME: what if the key does not exist?)
-int ioopm_hash_table_lookup(ioopm_hash_table_t *ht, char *key);
+bool ioopm_hash_table_lookup(ioopm_hash_table_t *ht, char *key, int *result);
 
 /// @brief remove any mapping from key to a value
 /// @param ht hash table operated upon
 /// @param key key to remove
 /// @return the value mapped to by key (FIXME: what if the key does not exist?)
 int ioopm_hash_table_remove(ioopm_hash_table_t *ht, char *key);
+
--- hash_table_tests.c
+++ hash_table_tests.c
@@ -30,6 +30,28 @@ void test_create_destroy()
    ioopm_hash_table_destroy(ht);
 }
 
+void test_insert_once()
+{
+  // create new hash table
+  ioopm_hash_table_t *ht = ioopm_hash_table_create();
+
+  char *key = "abc";
+  int value = 123;
+
+  // check that key is not in ht
+  int result = 0;
+  CU_ASSERT_FALSE(ioopm_hash_table_lookup(ht, key, &result));
+  CU_ASSERT_EQUAL(result, 0);
+
+  // insert key-value pair and check that the mapping exists
+  ioopm_hash_table_insert(ht, key, value);
+  CU_ASSERT_TRUE(ioopm_hash_table_lookup(ht, key, &result));
+  CU_ASSERT_EQUAL(result, value);
+
+  // destroy hash table
+  ioopm_hash_table_destroy(ht);
+}
+
 
 int main() {
   // First we try to set up CUnit, and exit if we fail
